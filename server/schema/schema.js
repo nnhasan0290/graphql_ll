7039fe31113897b8332bf441aade30a27a8e6faa
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-02-10 01:23:55 +0000

    more

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,10 @@
-const { clients } = require("./sampledata.js");
+const { clients, projects } = require("./sampledata.js");
 const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
   GraphQLID,
+  GraphQLList,
 } = require("graphql");
 
 const clientType = new GraphQLObjectType({
@@ -15,17 +16,41 @@ const clientType = new GraphQLObjectType({
     phone: { type: GraphQLString },
   }),
 });
+const projectType = new GraphQLObjectType({
+  name: "project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    description: { type: GraphQLString },
+    name: { type: GraphQLString },
+    clientId: { type: GraphQLID },
+    status: { type: GraphQLString },
+  }),
+});
 
 const RootSchema = new GraphQLObjectType({
   name: "rootQuery",
   description: "root query",
   fields: () => ({
+    projects: {
+      type: new GraphQLList(projectType),
+      resolve: () => projects,
+    },
+    clients: {
+      type: new GraphQLList(clientType),
+      resolve: () => clients,
+    },
     client: {
       type: clientType,
       args: { id: { type: GraphQLID } },
       resolve: (parent, args) => {
         return clients.find((client) => client.id === args.id);
       },
+    },
+    project: {
+      type: projectType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) =>
+        projects.find((project) => project.id === args.id),
     },
   }),
 });
